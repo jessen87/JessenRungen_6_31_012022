5c8ca22d27ead9b92bd2ba060e08d4f9e065d3d8
jessen87 <jessen.rungen87@gmail.com>
2022-02-19 02:09:58 +0100

    version soutenance

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -27,32 +27,14 @@ exports.getOneSauce = (req, res, next) => {
 };
 
 exports.modifySauces = (req, res, next) => {
-  if (req.file) {
-    Sauce.findOne({ _id: req.params.id })
-
-    .then((sauce) => {
-      // On supprime l'ancienne image du serveur
-      const filename = sauce.imageUrl.split('/images/')[1];
-      fs.unlinkSync(`images/${filename}`)
-    });
-
-    sauceObject = {
-      // On ajoute la nouvelle image
-      ...JSON.parse(req.body.sauce),//permet de récupérer le corps de la requêtte en json utilisable 
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-    };
-  } else {
-    // Si la modification ne contient pas de nouvelle image alors on modifie le corps de la requette
-    sauceObject = { ...req.body }
-  };
-
-  Sauce.updateOne(
-    // On applique les paramètre de sauceObject
-    { _id: req.params.id },
-    { ...sauceObject, _id: req.params.id }
-  )
-  .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
-  .catch((error) => res.status(400).json({ error }));
+  const saucesObject = req.file ?
+    {
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  Sauce.updateOne({ _id: req.params.id }, { ...saucesObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteSauces = (req, res, next) => {
